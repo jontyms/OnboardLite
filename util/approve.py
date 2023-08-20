dd8cc16b767b4a2ae7c20ea03080f0b6d8e8449e
Peyton Duncan <dev@peytond.me>
2023-08-19 23:21:21 -0700

    Discord HTTP to HTTPS

--- util/approve.py
+++ util/approve.py
@@ -1,12 +1,13 @@
 import json
 import os
-import yaml
+
 import boto3
 import requests
-from boto3.dynamodb.conditions import Key, Attr
+import yaml
+from boto3.dynamodb.conditions import Attr, Key
 
-from util.options import Options
 from util.horsepass import HorsePass
+from util.options import Options
 
 options = Options.fetch()
 
@@ -44,7 +45,7 @@ class Approve:
             "Content-Type": "application/json",
             "X-Audit-Log-Reason": "Hack@UCF OnboardLite Bot"
         }
-        requests.put(f"http://discordapp.com/api/guilds/{options.get('discord', {}).get('guild_id')}/members/{discord_id}", headers=headers)
+        requests.put(f"https://discordapp.com/api/guilds/{options.get('discord', {}).get('guild_id')}/members/{discord_id}", headers=headers)
 
         # Get DM channel ID to send later...
         get_channel_id_body = {
